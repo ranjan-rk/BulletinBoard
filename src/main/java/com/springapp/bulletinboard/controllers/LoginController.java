9e9e21a0a45d07bb85ed7bf8a6b421c05f2e08da
Alexander Gudulin <alexandr.gudulin@gmail.com>
2014-06-13 22:46:18 +0400

    Find users in DB on login

--- src/main/java/com/springapp/bulletinboard/controllers/LoginController.java
+++ src/main/java/com/springapp/bulletinboard/controllers/LoginController.java
@@ -1,14 +1,23 @@
 package com.springapp.bulletinboard.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String index(ModelMap model) {
+    @RequestMapping(method=RequestMethod.GET, value="/login")
+    public String displayLoginPage(){
+        return "login";
+    }
+
+    @RequestMapping(value="/login", params="error")
+    public String directToLoginPageWithError(Model model){
+        // Adding an attribute to flag that an error happened at login
+        model.addAttribute("loginFailed", true);
+
         return "login";
     }
 }
